c58e24fc61fae34a1e62cd460572f03b3ea0d9df
dapevi1997 <dapevi97@gmail.com>
2022-05-30 14:26:08 -0500

    librería underscore.js agregada

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,1 +1,70 @@
-console.log('Hello world');
+/**
+ * 2C = 'two of clubs 
+ * 2D = 'two of diaminds
+ * 2H = 'two of hearts
+ * 2S = 'two of spades
+ */
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+//esta función crea un nuevo deck
+const crearDeck = () => {
+
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo)
+        }
+    }
+
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp + tipo)
+        }
+    }
+    //console.log(deck);
+    deck = _.shuffle(deck);
+    console.log(deck);
+    return deck;
+}
+
+crearDeck();
+
+// esta funcion ,e permite tomar una carta
+
+const perdircarta = () => {
+    if (deck.length === 0) {
+        throw "No hay cartas en el deck";
+    }
+    let carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta);
+    return carta;
+
+
+}
+
+
+//perdircarta();
+
+const valorcarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ?
+        (valor === 'A') ? 11 : 10
+        : valor*1;
+    //let puntos =0;
+
+    // if (isNaN(valor)){
+    //     puntos = (valor === 'A') ? 11 : 10;
+    // }else {
+
+    //     puntos = valor*1;
+    // }
+    // console.log(puntos );
+
+}
+
+const valor = valorcarta(perdircarta());
+console.log({valor});
